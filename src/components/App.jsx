82be2f75bcc8ad42d573c3516d6d12e98aa98702
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-04-24 21:51:13 +0200

    all json files added in one folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from "components/Profile/Profile.jsx";
 import { Statistics } from "./Statistics/Statistics.jsx";
 import { FriendList } from "./FriendList/FriendList.jsx";
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory.jsx";
-import user from 'user.json';
-import data from 'data.json';
-import friends from 'friends.json';
-import transactions from 'transactions.json';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 import css from './App.module.css'
 
 export const App = () => {
